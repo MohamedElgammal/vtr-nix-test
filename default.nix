78490217aeec00d4c1c16fd2d5c46c401c38f040
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-05-16 21:54:20 +0200

    trial

--- default.nix
+++ default.nix
@@ -208,7 +208,7 @@ directed_moves_static =
         }).custom;
     in
       flag_sweep "directed_moves_sweep" test {
-        place_static_move_prob = [{100,0,0,0,0,0} {10,10,10,10,10,10}];
+        place_static_move_prob = ["{100,0,0,0,0,0}" "{10,10,10,10,10,10}"];
         inner_num = [0.125 0.25 0.5 1 2];
         seed = range 1 5;
       };
