db2fd0849e045245c9d3e805c766a8d84df07499
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-05-16 22:01:31 +0200

    Adding 2 tests
    1 for VPR8 and another for all directed moves with equal probabilities

--- default.nix
+++ default.nix
@@ -201,16 +201,26 @@ rec {
         seed = range 1 5;
       };
 
-directed_moves_static =
+VPR8 =
     let test = { flags, ...}: (mohameds_test {
-          flags = "--simpleRL_agent_placement off --pack --place ${flags_to_string flags}";
+          flags = "--simpleRL_agent_placement off --pack --place --place_static_move_prob {100,0,0,0,0,0} ${flags_to_string flags}";
           vtr = vtr_directed_moves;
         }).custom;
     in
       flag_sweep "directed_moves_sweep" test {
-        place_static_move_prob = ["{100,0,0,0,0,0}" "{10,10,10,10,10,10}"];
         inner_num = [0.125 0.25 0.5 1 2];
         seed = range 1 5;
       };
+      
+Equi_prob =
+    let test = { flags, ...}: (mohameds_test {
+          flags = "--simpleRL_agent_placement off --pack --place --place_static_move_prob {10,10,10,10,10,10} ${flags_to_string flags}";
+          vtr = vtr_directed_moves;
+        }).custom;
+    in
+      flag_sweep "directed_moves_sweep" test {
+        inner_num = [0.125 0.25 0.5 1 2];
+        seed = range 1 5;
+      };      
 
 }
