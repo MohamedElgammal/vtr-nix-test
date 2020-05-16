99da3e957f193e11b1952e5b202e2b44a2dd55ef
MohamedElgammal <mohamed.elgammal@mail.utoronto.ca>
2020-05-16 16:05:11 -0400

    adding nix job for default vpr8

---
+++ jobs/vpr8.nix
@@ -0,0 +1,8 @@
+{ ... }: # ignore arguments
+
+with import ../default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix, passing in nixpkgs
+
+# each attribute is a job
+{
+    VPR8 = VPR8.summary;
+}
