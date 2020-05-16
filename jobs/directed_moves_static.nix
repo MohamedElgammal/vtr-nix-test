e4cc4ad88d57235d3ac53ba30da1250637b92e91
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-05-16 22:02:23 +0200

    Update directed_moves_static.nix

--- jobs/directed_moves_static.nix
+++ jobs/directed_moves_static.nix
@@ -4,5 +4,5 @@ with import ../default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix
 
 # each attribute is a job
 {
-  directed_moves_static = directed_moves_static.summary;
+  Equi_prob = Equi_prob.summary;
 }
