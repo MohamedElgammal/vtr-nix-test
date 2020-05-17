3c31a1f9ce0b7f4c19ae5c7dcc1a929fb914638a
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-05-17 16:22:29 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -199,7 +199,7 @@ rec {
     variant = "rlim_option";
     url = "ssh://git@github.com/MohamedElgammal/directed_moves.git";
     ref = "rlim_option";
-    rev = "3d484f448a73f4f9c37f08e7f0cade0782874450";
+    rev = "d5e85c1f37cb1d2675a9c63230b72bf6e85ab487";
   };
   
   directed_moves_sweep =
