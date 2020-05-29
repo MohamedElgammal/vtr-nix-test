576b2cf35fd9ffa1dcc94ba8c44248818c7bc786
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-05-29 21:22:44 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -313,11 +313,11 @@ titan_sweep =
     let test = {flags, ...}:
           (make_regression_tests {
             vtr = vtr_egreedy;
-            flags = "--simpleRL_agent_placement on --pack --place --place_reward_num 2 --place_agent_gamma 0.05 --place_agent_epsilon 0.3 --place_dm_rlim 3  --place_agent_algorithm e_greedy ${flags_to_string flags}";
+            flags = "--inner_num 0.125 --simpleRL_agent_placement on --pack --place --route --place_reward_num 2 --place_agent_gamma 0.05 --place_agent_epsilon 0.3 --place_dm_rlim 3  --place_agent_algorithm e_greedy ${flags_to_string flags}";
           }).vtr_reg_nightly.titan_quick_qor;
     in
       flag_sweep "titan_sweep" test {
-        inner_num = [0.125 0.25 0.5 1.0];
+        #inner_num = [0.125 0.25 0.5 1.0];
         seed = range 1 3;
     };
     
@@ -325,11 +325,11 @@ titan_vpr =
     let test = {flags, ...}:
           (make_regression_tests {
             vtr = vtr_egreedy;
-            flags = "--simpleRL_agent_placement off --pack --place ${flags_to_string flags}";
+            flags = "--inner_num 1 --simpleRL_agent_placement off --pack --place --route ${flags_to_string flags}";
           }).vtr_reg_nightly.titan_quick_qor;
     in
       flag_sweep "titan_vpr" test {
-        inner_num = [0.125 0.25 0.5 1.0];
+        #inner_num = [0.125 0.25 0.5 1.0];
         seed = range 1 3;
     };
     
