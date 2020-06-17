07293e229241a5c258eb6087ec39efac6a52a282
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-06-17 14:49:15 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -262,7 +262,7 @@ rec {
         }).custom;
     in
       flag_sweep "branch_test" test {
-        place_static_move_prob =[ {0,100,0,0,0,0,0} ];
+        place_static_move_prob =[ '{0,100,0,0,0,0,0}' ];
         inner_num = [0.125 1.0];
         seed = range 1 3;
       };
