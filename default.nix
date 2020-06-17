9729be566eeadaceccfe8db9b53d9e0dde90e29e
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-06-17 03:51:05 +0200

    Update default.nix

--- default.nix
+++ default.nix
@@ -175,7 +175,8 @@ rec {
           qor_parse_file = "qor_large.txt";
           pass_requirements_file = "pass_requirements.txt";
           arch_list = ["k6_frac_N10_frac_chain_mem32K_40nm.xml"];
-          circuit_list = ["bgm.v" "LU8PEEng.v" "LU32PEEng.v" "mcml.v"  "stereovision0.v" "stereovision1.v" "stereovision2.v"];
+          circuit_list = ["bgm.v"];
+          #circuit_list = ["bgm.v" "LU8PEEng.v" "LU32PEEng.v" "mcml.v"  "stereovision0.v" "stereovision1.v" "stereovision2.v"];
           archs_dir = "arch/timing";
           circuits_dir = "benchmarks/verilog";
           script_params = "-track_memory_usage --routing_failure_predictor off";
@@ -262,7 +263,7 @@ rec {
         }).custom;
     in
       flag_sweep "branch_test" test {
-        place_static_move_prob = ["0,100,0,0,0,0,0" "50,50,0,0,0,0,0" "0,0,100,0,0,0,0" "50,0,50,0,0,0,0" "0,0,0,100,0,0,0" "50,0,0,50,0,0,0" "0,0,0,0,100,0,0" "50,0,0,0,50,0,0" "0,0,0,0,0,100,0" "50,0,0,0,0,50,0" "0,0,0,0,0,0,100" "50,0,0,0,0,0,50" "50,0,0,0,0,5,0" "50,0,0,0,0,0,5"];
+        place_static_move_prob = ["0,100,0,0,0,0,0"];
         inner_num = [0.125 1];
         seed = range 1 3;
       };
