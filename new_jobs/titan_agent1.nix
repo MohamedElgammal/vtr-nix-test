686f894ae4cfcc5dcbe08c9e06e516ff1708df2b
MohamedElgammal <47126408+MohamedElgammal@users.noreply.github.com>
2020-07-14 15:28:11 +0200

    Update titan_agent1.nix

--- new_jobs/titan_agent1.nix
+++ new_jobs/titan_agent1.nix
@@ -4,5 +4,6 @@ with import ../default.nix { pkgs = import <nixpkgs> {}; }; # import default.nix
 
 # each attribute is a job
 {
+    vtr_agent_1 = vtr_agent_1.summary;
     titan_agent_1 = titan_agent_1.summary;
 }
